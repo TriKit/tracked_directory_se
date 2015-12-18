c41baf887bfb6cbe5baad08cc7bc063ab5c84b89
Marsel Gabdulov <mars.gabdulov@gmail.com>
2015-12-18 13:32:31 +0300

    Add sort be direction and filter by name/extension to ls method

--- lib/tracked_directory.rb
+++ lib/tracked_directory.rb
@@ -6,33 +6,53 @@ class TrackedDirectory
     @path = path
   end
 
-  def ls(show_entries: :files)
+  def ls(show_entries: nil, filter_by_extension: nil, filter_by_name: nil, sort_direction: :asc)
     entries = Dir.entries(@path)
     entries.delete(".")
     entries.delete("..")
+
     # тремя способами
 
     # 1(изначальный)
-    # show_entries == :files
+    # if show_entries == :files
     #   entries.delete_if { |i| !File.file?("#{@path}/#{i}") }
     # else show_entries == :dirs
     #   entries.delete_if { |i| File.file?("#{@path}/#{i}") }
     # end
     # entries
-    #
-    # 2
+
+    #2 вариант
     # entries.delete_if { |i| !File.file?("#{@path}/#{i}")} if show_entries == :files
     # entries.delete_if { |i| File.file?("#{@path}/#{i}")} if show_entries == :dirs
     # entries
-    #
-    #3
+
+    #3 вариант
     entries.delete_if do |i|
       is_file = !File.file?("#{@path}/#{i}")
       (is_file if show_entries == :files) || (!is_file if show_entries == :dirs)
     end
+
+    #выводит ошибку, если введено что-то неправильное
+    if ![:files, :dirs, nil].include?(show_entries)
+      raise "show_entries parameter must be equal only files, dirs or be empty"
+    end
+
+    #sort by desc direction
+    if sort_direction == :desc
+      entries.sort! { |x,y| y <=> x }
+    else
+    #sort_by asc direction
+      entries.sort
+    end
+
+    #filter_by_extension
+    if filter_by_extension != nil
+      entries = entries.select { |i| i =~/\.#{filter_by_extension}/ }
+    end
+    #filter_by_name
+    entries = entries.select { |i| i =~/\A#{filter_by_name}/ }
     entries
-
-    #один должен быть коротким
   end
-
 end
+d1 = TrackedDirectory.new("/Users/marsel/rails_and_ruby/ruby_files/tracked_directory/spec/test/test_dir_1")
+p d1.ls(show_entries: :dirs, sort_direction: :desc)
